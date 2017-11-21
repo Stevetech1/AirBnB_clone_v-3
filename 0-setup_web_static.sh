cf11e213539560cb9002666c157567c0faa84191
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 05:44:13 +0000

    added -y

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,10 @@
 # script that creates a folder if one doesn't already exist
 
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/;
 sudo mkdir -p /data/web_static/releases/test;
-sudo echo "testing" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "37i location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}" /etc/nginx/sites-available/default
