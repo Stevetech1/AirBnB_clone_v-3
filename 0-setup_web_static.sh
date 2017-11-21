f7c572f1358a87a0534eb7b009161beb2c4ccf03
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 06:20:16 +0000

    added nginx start

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
+sudo service nginx start
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
