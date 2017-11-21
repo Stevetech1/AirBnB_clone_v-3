59e0cae7b5b64f24720203a2649e7b9b33f6a868
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 03:32:54 +0000

    added sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,6 @@ sudo mkdir -p /data/web_static/shared/;
 sudo mkdir -p /data/web_static/releases/test;
 echo "testing" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown -hR ubuntu:ubuntu /data/web_static/releases/test/index.html
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "39i location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
