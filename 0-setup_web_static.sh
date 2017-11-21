996ec9dcd5bd50f1b4467c1472177b3c911857e3
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 02:21:46 +0000

    added link and chown

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,9 @@
 
 sudo apt-get update
 sudo apt-get install nginx
+sudo mkdir -p /data/web_static/shared/;
 sudo mkdir -p /data/web_static/releases/test;
-sudo touch /data/web_static/releases/test/index.html
+echo "testing" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -hR ubuntu:ubuntu /data/web_static/releases/test/index.html
 sudo service nginx restart
