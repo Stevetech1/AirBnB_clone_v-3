46544e375379342d139869f704d5e2418f9a8476
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 04:55:38 +0000

    added \t in front of location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install nginx
 sudo mkdir -p /data/web_static/shared/;
 sudo mkdir -p /data/web_static/releases/test;
-echo "testing" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo echo "testing" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "39i location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "37i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
