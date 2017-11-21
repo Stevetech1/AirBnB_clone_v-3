16a07aafbd062873bc5659aa13ed45a129ebd1c1
jvpupcat <vuong.joann@yahoo.com>
2017-11-21 05:09:00 +0000

    removed semicolon

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/releases/test;
 sudo echo "testing" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "37i location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "37i location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
