f710d1434275305a6956af1d8f0c69448ab98ba6
jvpupcat <vuong.joann@yahoo.com>
2017-12-13 12:04:47 +0000

    added comments, fixed pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-# script that starts a Flask web application
+"""script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
+    """def to say Hello HBNB with use of slash"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
+    """def to say HBNB with use of /hbnb"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """def to display C and text after"""
     return 'C %s' % text.replace('_', ' ')
 
 if __name__ == '__main__':
