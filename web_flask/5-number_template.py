8674e5b6b485bb29ec6e112c99f523921f745373
jvpupcat <vuong.joann@yahoo.com>
2017-12-13 12:13:36 +0000

    fixed pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,29 +5,36 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 def python_defaultmsg():
     return 'Python is cool'
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return 'Python %s' % text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def if_number(n):
     return '%d is a number' % n
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
