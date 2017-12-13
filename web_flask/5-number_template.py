6c839ca0b9eefc0724246301018bab2a7cc5699d
jvpupcat <vuong.joann@yahoo.com>
2017-12-13 08:37:23 +0000

    added int:

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -29,7 +29,7 @@ def python_text(text):
 def if_number(n):
     return '%d is a number' % n
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', number=n)
 
