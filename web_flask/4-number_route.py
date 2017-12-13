7045f2a7542b01eedb676c14e024e51736a48f1a
jvpupcat <vuong.joann@yahoo.com>
2017-12-13 12:10:14 +0000

    fixed pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,25 +4,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
 def python_defaultmsg():
     return 'Python is cool'
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     return 'Python %s' % text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def if_number(n):
