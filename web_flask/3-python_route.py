8d89e447674c501bf587543593f0e0c064922b83
jvpupcat <vuong.joann@yahoo.com>
2017-12-13 12:08:05 +0000

    fixed pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,21 +4,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_HBNB():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     return 'C %s' % text.replace('_', ' ')
 
+
 @app.route('/python', strict_slashes=False)
 def python_defaultmsg():
     return 'Python is cool'
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
